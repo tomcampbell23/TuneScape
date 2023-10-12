efb47f35423f07ddb0b1673304310219ca51dfa9
Tom Campbell <65591392+tomcampbell23@users.noreply.github.com>
2023-10-12 11:58:34 +1000

    update

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from __future__ import print_function
 from flask import Flask, render_template, jsonify
-import os, sys
+import os, sys, json
 
 app = Flask(__name__)
 
@@ -14,7 +14,7 @@ def list_files():
         file_list[music_list[count]] = os.listdir(directory_path + '/' + str(dir))
         count +=1
     print(file_list, file=sys.stderr)
-    return render_template('index.html', files=file_list)
+    return render_template('index.html', files=json.dumps(file_list))
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/script.js
+++ static/script.js
@@ -1,15 +1,33 @@
-//   for (s in Sunshine) {
-    //       console.log(s)
-    //       playlist.push(s.name)
-    //   }
-
 const player = document.getElementById('player')
+var current = 0
 
 function nextsong() {
-    document.getElementById('song').src = "../static/Music/SunshineCoast/Bottles of Thrill.mp3"
+    if (playlist.length > current + 1) {
+        current += 1
+    } else {
+        current = 0
+    }
+    console.log("../static/Music/SunshineCoast/" + playlist[current])
+    document.getElementById('song').src = "../static/Music/SunshineCoast/" + playlist[current]
     player.load()
     player.play()
 }
 
-var musicFiles = document.getElementById('data').textContent
-console.log(musicFiles)
+function shuffle(array) { 
+    return array.sort(() => Math.random() - 0.5); 
+}
+
+function fileslist() {
+    var musicFiles = JSON.parse(document.getElementById('data').textContent)
+    return musicFiles
+}
+
+var musicFiles = fileslist()
+
+/*for (i in musicFiles) {
+    var location1 = 'SunshineCoast'
+} */
+
+var location1 = 'SunshineCoast'
+
+var playlist = shuffle(Object.values(musicFiles[location1]))
